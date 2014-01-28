gruntjs/grunt-contrib-sass

commit 35ad9cd45e73bef35b256ebfd8a46506c38439c8

Fix spawning on Windows

tasks/sass.js
@@ -11,6 +11,7 @@ var dargs = require('dargs');
 var numCPUs = require('os').cpus().length;
 var async = require('async');
 var chalk = require('chalk');
+var spawn = require('win-spawn');
 
 module.exports = function (grunt) {
   var bannerCallback = function (filename, banner) {
@@ -58,12 +59,6 @@ module.exports = function (grunt) {
         '--load-path', path.dirname(src)
       ].concat(passedArgs);
 
-      if (process.platform === 'win32') {
-        args.unshift('sass.bat');
-      } else {
-        args.unshift('sass');
-      }
-
       if (bundleExec) {
         args.unshift('bundle', 'exec');
       }
@@ -76,13 +71,13 @@ module.exports = function (grunt) {
       // Make sure grunt creates the destination folders
       grunt.file.write(file.dest, '');
 
-      grunt.util.spawn({
-        cmd: args.shift(),
-        args: args,
-        opts: {
-          stdio: 'inherit'
-        }
-      }, function (error, result, code) {
+      var cp = spawn('sass', args, {stdio: 'inherit'});
+
+      cp.on('error', function (err) {
+        grunt.warn(err);
+      });
+
+      cp.on('close', function (code) {
         if (code === 127) {
           return grunt.warn(
             'You need to have Ruby and Sass installed and in your PATH for\n' +
@@ -91,13 +86,17 @@ module.exports = function (grunt) {
           );
         }
 
+        if (code > 0) {
+          return grunt.warn('Exited with error code ' + code);
+        }
+
         // Callback to insert banner
         if (banner) {
           bannerCallback(file.dest, banner);
         }
 
         grunt.log.writeln('File ' + chalk.cyan(file.dest) + ' created.');
-        next(error);
+        next();
       });
     }, cb);
   });

commit 8682828b3750ac2aa8abd197d8c5948250603bc1

Fix regression in bundler support
Commit 35ad9cd45e73 introduced a regression that breaks bundler support. This occurs because rather than spawning `bundle exec sass ...` it would now spawn `sass bundle exec ...`.

tasks/sass.js
@@ -59,8 +59,11 @@ module.exports = function (grunt) {
         '--load-path', path.dirname(src)
       ].concat(passedArgs);
 
+      var bin = 'sass';
+
       if (bundleExec) {
-        args.unshift('bundle', 'exec');
+        bin = 'bundle';
+        args.unshift('exec', 'sass');
       }
 
       // If we're compiling scss or css files
@@ -71,7 +74,7 @@ module.exports = function (grunt) {
       // Make sure grunt creates the destination folders
       grunt.file.write(file.dest, '');
 
-      var cp = spawn('sass', args, {stdio: 'inherit'});
+      var cp = spawn(bin, args, {stdio: 'inherit'});
 
       cp.on('error', function (err) {
         grunt.warn(err);