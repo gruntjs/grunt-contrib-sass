45a389fa8871d67a8ec83c6896e0ea69bf23bd6a
XhmikosR <xhmikosr@gmail.com>
2015-10-12 09:56:11 +0300

    JSHint: Check subfolders too.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
       },
       all: [
         'Gruntfile.js',
-        'tasks/*.js',
+        'tasks/**/*.js',
         '<%= nodeunit.tests %>'
       ]
     },
